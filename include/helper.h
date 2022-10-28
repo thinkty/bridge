debec305ddbafb8acf000f09ac2f04dd3736f303
thinkty <the.thinkty@gmail.com>
2022-10-28 12:59:07 -0400

    Update

--- include/helper.h
+++ include/helper.h
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#ifndef __TCP_HELPER_H
+#define __TCP_HELPER_H
 
-/* 
- * File:   helper.h
- * Author: wang3623 inspired by abc1986
- *
- * Created on September 8, 2018, 3:49 PM
- */
-
-#ifndef HELPER_H
-#define HELPER_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -26,18 +14,11 @@
 #include <pthread.h>
 #include <string.h>
 
-#define LISTEN_Q_LEN    5
-#define CMD_OPEN        "OPEN"
-#define CMD_READ        "READ"
-#define CMD_BACK        "BACK"
-#define CMD_CLOS        "CLOS"
-#define INVALID_CMD     "Invalid command\n"
-#define BUFF_SIZE       1500
-#define CMD_SIZE        5
+#define LISTEN_Q_LEN    (5)
 
 int h_listen(unsigned short);
 int h_accept(int);
 int h_connect(const char *, const char *);
 
-#endif /* HELPER_H */
+#endif
 
