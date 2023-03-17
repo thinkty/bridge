ab5e5187ca555934a432d5953e3cf9196f2e7005
thinkty <the.thinkty@gmail.com>
2023-03-16 22:05:17 -0400

    Added hash map implementation with linear probing

--- include/table.h
+++ include/table.h
@@ -1,48 +1,144 @@
 #ifndef BRIDGE_TABLE_H
 #define BRIDGE_TABLE_H
 
-#include <pthread.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "util.h"
 
-#define ENTRY_NAME_LEN (20)
+#define MIN(X,Y) (X < Y ? X : Y)
+
+#define TABLE_TOPIC_LEN       (7)
+#define TABLE_INITIAL_SIZE    (10)
+#define TABLE_HASH_FNV_PRIME  (0x100000001b3)
+#define TABLE_HASH_FNS_OFFSET (0xcbf29ce484222325)
 
 /**
-    TODO:
- * @brief Table keeping track of subscriber entries
+ * @brief Subscriber doubly-linked list.
  * 
- * @param lock Mutex lock for the name-address table
+ * @param next The next subscriber subscribed to the same topic
+ * @param prev The previous subscriber
+ * @param ip IP address of the requester
+ * @param port Port number of the requester
+ * @param csock Socket file descriptor for the subscribed client
+ */
+typedef struct subscriber {
+    struct subscriber * next;
+    struct subscriber * prev;
+    uint32_t ip;
+    uint16_t port;
+    int csock;
+} subscriber_t;
+
+/**
+ * @brief Intermediate data structure to store the subscriber list at given topic.
+ * 
+ * @param topic The corresponding topic
+ * @param subscriber Linked list of subscriber(s)
+ */
+typedef struct topic {
+    char str[TABLE_TOPIC_LEN];
+    subscriber_t * subscriber;
+} topic_t;
+
+/**
+ * @brief Table keeping track of subscriber entries.
+ * 
+ * @param lock Mutex lock for the pub/sub table
+ * @param map The array of entries that correspond to the given topic
+ * @param map_size The size of the table (map)
+ * @param num_topics Number of entries in the table (map)
  */
 typedef struct {
 	pthread_mutex_t * lock;
+    topic_t ** map;
+    uint64_t map_size;
+    uint64_t num_topics;
 } table_t;
 
 /**
- * @brief An entry in the table
+ * @brief Allocate and initialize the pub/sub table
  * 
- * @param ip IP address of the requester
- * @param port Port number of the requester
- * @param topics Topics currently subscribed by the requester
+ * @returns pointer to the allocated table.
  */
-typedef struct entry {
-    uint32_t ip;
-    uint16_t port;
-    char topics[ENTRY_NAME_LEN]; // TODO:
-} entry_t;
+table_t * init_table(void);
 
 /**
- * @brief Initialize the pub/sub table (mutexes, etc.)
+ * @brief Calculate the hash given the topic string using the FNV-1a hash
+ * algorithm.
  * 
- * @returns OK on success and ERR on failure.
+ * @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+ * 
+ * @param topic_str The topic string with length less than or equal to set topic size
+ * @param map_size The size of the map
+ * 
+ * @returns An unsigned integer to be used to get the correct topic in the table.
  */
-int init_table(table_t * table);
+uint64_t hash(char * topic_str, uint64_t map_size);
 
+/**
+ * @brief Query the table for the given topic.
+ * 
+ * @param table Table to query on
+ * @param topic_str Topic string
+ * 
+ * @returns The topic or NULL if the topic does not exist.
+ */
+topic_t * get_topic(table_t * table, char * topic_str);
 
+/**
+ * @brief Insert the new topic into the map. If the topic already exists, return
+ * the topic. In collision, use linear probing. If the map is full, double the
+ * size and re-insert.
+ * 
+ * @param table Table to insert to
+ * @param topic_str Topic string
+ * 
+ * @returns The topic or NULL if an error has occurred.
+*/
+topic_t * set_topic(table_t * table, char * topic_str);
 
+/**
+ * @brief A helper function to insert a given topic object into the hash map.
+ * The function assumes that the parameters are valid and the map has enough
+ * space to insert the topic using linear probing.
+ * 
+ * @param table Table to insert to
+ * @param topic Topic object
+ */
+void insert_topic(table_t * table, topic_t * topic);
 
-void subscribe(table_t * table, entry_t entry);
-void unsubscribe(table_t * table, entry_t entry);
-void publish(table_t * table, entry_t entry);
+/**
+ * @brief Add the subscriber to the topic. If a topic does not exist, insert the
+ * new topic and then add the new subscriber.
+ * 
+ * @param table The table to insert to
+ * @param topic_str The topic to insert the new subscriber to
+ * @param new_sub The new subscriber to insert
+ * 
+ * @returns Positive value if it already exists in the table. OK if successfully
+ * inserted. ERR if error while inserting to the table.
+ */
+int insert_sub(table_t * table, char * topic_str, subscriber_t * new_sub);
+
+/**
+ * @brief Remove the subscriber from the topic. If a topic does not exist or the
+ * subscriber does not exist, ignore.
+ * 
+ * @param table The table to remove from
+ * @param topic_str The topic to remove the subscriber from
+ * @param sub Temporary subscriber info to match when searching
+*/
+void remove_sub(table_t * table, char * topic_str, subscriber_t sub);
+
+/**
+ * @brief Clean up the table and free the topics, subscribers, etc.
+ * 
+ * @param table Table to clean
+ */
+void cleanup_table(table_t * table);
 
 #endif
--- src/table.c
+++ src/table.c
@@ -1,12 +1,257 @@
 #include "table.h"
 
-int init_table(table_t * table)
+table_t * init_table()
 {
-	/* Initialize the table mutex lock */
-	if (pthread_mutex_init(table->lock, NULL)) {
-		perror("pthread_mutex_init(table->lock)");
-        return ERR;
+	/* Allocate table */
+	table_t * table = malloc(sizeof(table_t));
+	if (table == NULL) {
+		return NULL;
 	}
 
-    return OK;
+	/* Initialize the table mutex lock */
+	table->lock = malloc(sizeof(pthread_mutex_t));
+	if (table->lock == NULL) {
+		return NULL;
+	}
+	if (pthread_mutex_init(table->lock, NULL)) {
+		perror("pthread_mutex_init(table->lock)");
+        return NULL;
+	}
+
+	/* Initialize the map */
+	table->num_topics = 0;
+	table->map_size = TABLE_INITIAL_SIZE;
+	table->map = malloc(sizeof(topic_t *) * table->map_size);
+	if (table->map == NULL) {
+		return NULL;
+	}
+
+	/* Initialize each topic in the map to NULL */
+	for (int i = 0; i < table->map_size; i++) {
+		table->map[i] = NULL;
+	}
+
+    return table;
+}
+
+uint64_t hash(char * topic_str, uint64_t map_size)
+{
+	uint64_t hash = TABLE_HASH_FNS_OFFSET;
+
+	for (int i = 0; i < MIN(strlen(topic_str), TABLE_TOPIC_LEN); i++) {
+		hash ^= (uint64_t)topic_str[i];
+		hash *= TABLE_HASH_FNV_PRIME;
+	}
+
+	return hash % map_size;
+}
+
+topic_t * get_topic(table_t * table, char * topic_str)
+{
+	uint64_t index = hash(topic_str, table->map_size);
+
+	/* If it breaks from the while loop, topic does not exist */
+	while (table->map[index] != NULL) {
+
+		if (strcmp(table->map[index]->str, topic_str) == 0) {
+			return table->map[index];
+		}
+
+		/* Using linear probing */
+		index++;
+		if (index >= table->map_size) {
+			index = 0;
+		}
+	}
+
+	return NULL;
+}
+
+topic_t * set_topic(table_t * table, char * topic_str)
+{
+	/* If the topic already exists, return it instead */
+	topic_t * topic;
+	if ((topic = get_topic(table, topic_str)) != NULL) {
+		return topic;
+	}
+
+	/* Allocate new topic to insert */
+	topic = malloc(sizeof(topic_t));
+	if (topic == NULL) {
+		return NULL;
+	}
+	topic->subscriber = NULL;
+	strncpy(topic->str, topic_str, TABLE_TOPIC_LEN);
+
+	/* If table is full, double the table size and re-insert */
+	if (table->num_topics + 1 >= table->map_size) {
+		topic_t ** new_map = malloc(sizeof(topic_t *) * (table->map_size * 2));
+		if (new_map == NULL) {
+			return NULL;
+		}
+
+		topic_t ** old_map = table->map;
+		uint64_t old_map_size = table->map_size;
+
+		table->map = new_map;
+		table->map_size = table->map_size * 2;
+		table->num_topics = 0;
+		
+		/* Re-insert previous topics */
+		for (int i = 0; i < old_map_size; i++) {
+			if (old_map[i] != NULL) {
+				insert_topic(table, old_map[i]);
+			}
+		}
+		
+		/* Free the old map */
+		free(old_map);
+	}
+
+	insert_topic(table, topic);
+	return topic;
+}
+
+void insert_topic(table_t * table, topic_t * topic)
+{
+	uint64_t index = hash(topic->str, table->map_size);
+
+	for(;;) {
+
+		/* Empty slot found */
+		if (table->map[index] == NULL) {
+			table->map[index] = topic;
+			table->num_topics++;
+			return;
+		}
+
+		/* If not empty, collision. Use linear probing */
+		index++;
+		if (index >= table->map_size) {
+			index = 0;
+		}
+	}	
+}
+
+int insert_sub(table_t * table, char * topic_str, subscriber_t * new_sub)
+{
+	/* Get the topic or create if it does not exist */
+	topic_t * topic = set_topic(table, topic_str);
+	if (topic == NULL) {
+		return ERR;
+	}
+
+	/* First subscriber to add */
+	if (topic->subscriber == NULL) {
+		topic->subscriber = new_sub;
+		new_sub->prev = NULL;
+		new_sub->next = NULL;
+		return OK;
+	}
+
+	/* Iterate to the end and check if it already exists in the list */
+	subscriber_t * iter = topic->subscriber;
+	for (;;) {
+		/* Already subscribed */
+		if (iter->csock == new_sub->csock && iter->ip == new_sub->ip && iter->port == new_sub->port) {
+			return 1;
+		}
+		/* Last subscriber */
+		if (iter->next == NULL) {
+			break;
+		}
+		iter = iter->next;
+	}
+
+	/* Append to the end of the subscriber list */
+	iter->next = new_sub;
+	new_sub->prev = iter;
+	new_sub->next = NULL;
+	return OK;
+}
+
+void remove_sub(table_t * table, char * topic_str, subscriber_t sub)
+{
+	topic_t * topic = get_topic(table, topic_str);
+	if (topic == NULL || topic->subscriber == NULL) {
+		return;
+	}
+
+	/* Iterate to the given subscriber */
+	subscriber_t * iter = topic->subscriber;
+	for (;;) {
+		if (iter == NULL) {
+			break;
+		}
+		if (iter->csock == sub.csock && iter->ip == sub.ip && iter->port == sub.port) {
+			break;
+		}
+		iter = iter->next;
+	}
+
+	/* Subscriber not found in given topic */
+	if (iter == NULL) {
+		return;
+	}
+
+	/* Unlink the subscriber and free it */
+	if (iter->prev == NULL && iter->next == NULL) {
+		topic->subscriber = NULL;
+	} else if (iter->prev == NULL) {
+		topic->subscriber = iter->next;
+	} else if (iter->next == NULL) {
+		iter->prev->next = NULL;
+	} else {
+		iter->prev->next = iter->next;
+		iter->next->prev = iter->prev;
+	}
+
+	free(iter);
+}
+
+void cleanup_table(table_t * table)
+{
+	if (table == NULL) {
+		return;
+	}
+
+	if (table->lock != NULL) {
+		pthread_mutex_destroy(table->lock);
+		free(table->lock);
+		table->lock = NULL;
+	}
+
+	if (table->map != NULL) {
+
+		/* Free the sbuscribers in each topic and the topic itself */
+		for (int i = 0; i < table->map_size; i++) {
+
+			if (table->map[i] == NULL) {
+				continue;
+			}
+
+			subscriber_t * list = table->map[i]->subscriber;
+			while (list != NULL) {
+				/* If last item, free itself */
+				if (list->next == NULL) {
+					free(list);
+					list = NULL;
+				} else {
+					list = list->next;
+					free(list->prev);
+					list->prev = NULL;
+				}
+
+				/* Close the client socket */
+				close(list->csock);
+			}
+
+			free(table->map[i]);
+			table->map[i] = NULL;
+		}
+		free(table->map);
+		table->map = NULL;
+	}
+
+	free(table);
 }
